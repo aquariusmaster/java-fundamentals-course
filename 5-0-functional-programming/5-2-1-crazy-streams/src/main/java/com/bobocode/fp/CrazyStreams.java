21275bb473419bdefa04e33298a7bc83e5a97308
Andrii Bobrov <Andrii.Bobrov_rft@refinitiv.com>
2021-09-15 15:59:58 +0300

    Homework#8: CrazyStreams - play with a Collector

--- 5-0-functional-programming/5-2-1-crazy-streams/src/main/java/com/bobocode/fp/CrazyStreams.java
+++ 5-0-functional-programming/5-2-1-crazy-streams/src/main/java/com/bobocode/fp/CrazyStreams.java
@@ -9,9 +9,14 @@ import lombok.AllArgsConstructor;
 import java.math.BigDecimal;
 import java.time.Month;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.BinaryOperator;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static com.bobocode.model.Sex.MALE;
 import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.*;
 
@@ -35,6 +40,20 @@ public class CrazyStreams {
         return accounts.stream().max(Comparator.comparing(Account::getBalance));
     }
 
+    public Map<Sex, Account> findRichestPerson2() {
+        return accounts.stream()
+                .collect(
+                        toMap(Account::getSex, identity(), BinaryOperator.maxBy(Comparator.comparing(Account::getBalance)))
+                );
+    }
+
+    public Map<Month, Account> findRichestPerson3() {
+        return accounts.stream()
+                .collect(
+                        toMap(account -> account.getBirthday().getMonth(), identity(), BinaryOperator.maxBy(Comparator.comparing(Account::getBalance)))
+                );
+    }
+
     /**
      * Returns a {@link List} of {@link Account} that have a birthday month equal to provided.
      *
@@ -54,7 +73,7 @@ public class CrazyStreams {
      * @return a map where key is true or false, and value is list of male, and female accounts
      */
     public Map<Boolean, List<Account>> partitionMaleAccounts() {
-        return accounts.stream().collect(groupingBy(a -> a.getSex() == Sex.MALE));
+        return accounts.stream().collect(partitioningBy(a -> a.getSex() == MALE));
     }
 
     /**
@@ -112,7 +131,9 @@ public class CrazyStreams {
      * @return true if there is an account that has an email with provided domain
      */
     public boolean containsAccountWithEmailDomain(String emailDomain) {
-        return accounts.stream().anyMatch(account -> account.getEmail().endsWith(emailDomain));
+        return accounts.stream()
+                .map(Account::getEmail)
+                .anyMatch(email -> email.endsWith(emailDomain));
     }
 
     /**
@@ -189,10 +210,11 @@ public class CrazyStreams {
     public Map<Month, BigDecimal> groupTotalBalanceByCreationMonth() {
         return accounts.stream()
                 .collect(
-                        groupingBy(
-                                account -> account.getCreationDate().getMonth(),
-                                mapping(Account::getBalance, reducing(BigDecimal.ZERO, BigDecimal::add))
-                        )
+                        toMap(account -> account.getCreationDate().getMonth(), Account::getBalance, BigDecimal::add)
+//                        groupingBy(
+//                                account -> account.getCreationDate().getMonth(),
+//                                mapping(Account::getBalance, reducing(BigDecimal.ZERO, BigDecimal::add))
+//                        )
                 );
     }
 
@@ -207,7 +229,8 @@ public class CrazyStreams {
                 .map(Account::getFirstName)
                 .flatMapToInt(String::chars)
                 .mapToObj(c -> (char) c)
-                .collect(groupingBy(identity(), counting()));
+                .collect(toMap(identity(), v -> 1L, Long::sum)); // not optimal bc a lot of boxing/unboxing ops
+//                .collect(groupingBy(identity(), counting()));
     }
 
     /**
@@ -220,10 +243,24 @@ public class CrazyStreams {
     public Map<Character, Long> getCharacterFrequencyIgnoreCaseInFirstAndLastNames(int nameLengthBound) {
         return accounts.stream()
                 .flatMap(account -> Stream.of(account.getFirstName(), account.getLastName()))
+                .filter(name -> name.length() >= nameLengthBound)
                 .map(String::toLowerCase)
                 .flatMapToInt(String::chars)
                 .mapToObj(c -> (char) c)
-                .collect(groupingBy(identity(), counting()));
+                .collect(groupingBy(
+                                identity(),
+//                                counting()
+                                collectingAndThen( // Just for fun: playing with finisher
+                                        Collector.of(
+                                                AtomicInteger::new,
+                                                (accumulator, character) -> accumulator.incrementAndGet(),
+                                                (left, right) -> left, //non-parallel stream since there is nothing to combine
+                                                AtomicInteger::get
+                                        ),
+                                        Long::valueOf // convert int to long
+                                )
+                        )
+                );
     }
 
 }
