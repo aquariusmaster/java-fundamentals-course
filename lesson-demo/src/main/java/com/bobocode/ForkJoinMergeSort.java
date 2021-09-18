0d4242750a4ea62866a0640fa2a79c26f70cfd82
Andrii Bobrov <verydrybeaver@gmail.com>
2021-09-18 18:54:30 +0300

    HOMEWORK #9 – CONCURRENCY - refactor

--- lesson-demo/src/main/java/com/bobocode/ForkJoinMergeSort.java
+++ lesson-demo/src/main/java/com/bobocode/ForkJoinMergeSort.java
@@ -6,27 +6,38 @@ import java.util.Arrays;
 import java.util.LongSummaryStatistics;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
+import java.util.concurrent.RecursiveTask;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class ForkJoinMergeSort {
 
     public static void main(String[] args) {
         ForkJoinPool forkJoinPool = ForkJoinPool.commonPool();
+        Supplier<int[]> arraySupplier = () -> ThreadLocalRandom.current().ints().limit(20_000_000).toArray();
         System.out.println("Concurrent merge sort");
-        LongSummaryStatistics stat =
-                Stream.iterate(1, i -> i <= 10, i -> ++i)
-                        .mapToLong(i -> {
-                            int[] arr = ThreadLocalRandom.current().ints().limit(20_000_000).toArray();
+        LongSummaryStatistics actionStat =
+                Stream.generate(arraySupplier)
+                        .limit(10)
+                        .mapToLong(arr -> {
                             long start = System.currentTimeMillis();
                             MergeSortAction main = new MergeSortAction(arr);
                             forkJoinPool.invoke(main);
                             return System.currentTimeMillis() - start;
                         })
-                        .peek(System.out::println)
+                        .peek(duration -> System.out.println(duration + "ms"))
                         .summaryStatistics();
 
-        System.out.println(stat);
+        System.out.println(actionStat);
+        LongSummaryStatistics taskStat =
+                Stream.generate(arraySupplier)
+                        .limit(10)
+                        .map(MergeSortTask::new)
+                        .mapToLong(forkJoinPool::invoke)
+                        .peek(duration -> System.out.println(duration + "ms"))
+                        .summaryStatistics();
+        System.out.println(taskStat);
     }
 
     @RequiredArgsConstructor
@@ -41,24 +52,59 @@ public class ForkJoinMergeSort {
             int[] right = Arrays.copyOfRange(arr, n, arr.length);
 
             MergeSortAction leftAction = new MergeSortAction(left);
-            MergeSortAction rightAction = new MergeSortAction(right);
             leftAction.fork();
+            MergeSortAction rightAction = new MergeSortAction(right);
             rightAction.compute();
             leftAction.join();
             merge(arr, left, right);
         }
+
+        private void merge(int[] arr, int[] left, int[] right) {
+            int i = 0, j = 0;
+            while (i < left.length && j < right.length) {
+                if (left[i] > right[j]) {
+                    arr[i + j] = right[j++];
+                } else {
+                    arr[i + j] = left[i++];
+                }
+            }
+            System.arraycopy(left, i, arr, i + j, left.length - i);
+            System.arraycopy(right, j, arr, i + j, right.length - j);
+        }
     }
 
-    private static void merge(int[] arr, int[] left, int[] right) {
-        int i = 0, j = 0;
-        while (i < left.length && j < right.length) {
-            if (left[i] > right[j]) {
-                arr[i + j] = right[j++];
-            } else {
-                arr[i + j] = left[i++];
-            }
+    @RequiredArgsConstructor
+    static class MergeSortTask extends RecursiveTask<Long> {
+        private final int[] arr;
+
+        @Override
+        protected Long compute() {
+            var start = System.currentTimeMillis();
+            if (arr.length < 2) return System.currentTimeMillis() - start;
+            var mid = arr.length / 2;
+            var left = Arrays.copyOfRange(arr, 0, mid);
+            var right = Arrays.copyOfRange(arr, mid, arr.length);
+
+            var leftAction = new MergeSortAction(left);
+            leftAction.fork();
+            var rightAction = new MergeSortAction(right);
+            rightAction.compute();
+            leftAction.join();
+            merge(arr, left, right);
+            return System.currentTimeMillis() - start;
         }
-        System.arraycopy(left, i, arr, i + j, left.length - i);
-        System.arraycopy(right, j, arr, i + j, right.length - j);
+
+        private void merge(int[] arr, int[] left, int[] right) {
+            int i = 0, j = 0;
+            while (i < left.length && j < right.length) {
+                if (left[i] > right[j]) {
+                    arr[i + j] = right[j++];
+                } else {
+                    arr[i + j] = left[i++];
+                }
+            }
+            System.arraycopy(left, i, arr, i + j, left.length - i);
+            System.arraycopy(right, j, arr, i + j, right.length - j);
+        }
     }
 }
--- lesson-demo/src/main/java/com/bobocode/Sorting.java
+++ lesson-demo/src/main/java/com/bobocode/Sorting.java
@@ -3,21 +3,23 @@ package com.bobocode;
 import java.util.Arrays;
 import java.util.LongSummaryStatistics;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class Sorting {
     public static void main(String[] args) {
         System.out.println("CPU number: " + Runtime.getRuntime().availableProcessors());
         System.out.println("Regular merge sort");
+        Supplier<int[]> arraySupplier = () -> ThreadLocalRandom.current().ints().limit(20_000_000).toArray();
         LongSummaryStatistics stat =
-                Stream.iterate(1, i -> i <= 10, i -> ++i)
-                        .mapToLong(i -> {
-                            int[] arr = ThreadLocalRandom.current().ints().limit(20_000_000).toArray();
+                Stream.generate(arraySupplier)
+                        .limit(10)
+                        .mapToLong(arr -> {
                             long start = System.currentTimeMillis();
                             mergeSort(arr);
                             return System.currentTimeMillis() - start;
                         })
-                        .peek(System.out::println)
+                        .peek(duration -> System.out.println(duration + "ms"))
                         .summaryStatistics();
 
         System.out.println(stat);
