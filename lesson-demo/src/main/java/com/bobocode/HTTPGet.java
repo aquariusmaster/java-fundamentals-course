07ddf77440f6fffe435db921108350b323d4c438
Andrii Bobrov <Andrii.Bobrov_rft@refinitiv.com>
2021-09-27 16:16:16 +0300

    HOMEWORK #11 – Introduction to Java Web Development
    - Given the URL: https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY. Implement the logic that performs HTTP call, parses the response and prints all image URLs to the console, so the output looks like the screenshot example.
    - Take a screenshot of your code :exclamation: and post it as a response to this message.
    - (Advanced) Create two more different implementations of item 1.

--- lesson-demo/src/main/java/com/bobocode/HTTPGet.java
+++ lesson-demo/src/main/java/com/bobocode/HTTPGet.java
@@ -2,12 +2,14 @@ package com.bobocode;
 
 import lombok.SneakyThrows;
 
-import java.net.HttpURLConnection;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,18 +18,34 @@ public class HTTPGet {
     @SneakyThrows
     public static void main(String[] args) {
         var link = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY";
+        getByHttpClient(link).forEach(System.out::println);
+        getByURLConnection(link).forEach(System.out::println);
+    }
+
+    @SneakyThrows
+    public static List<String> getByHttpClient(String url) {
         HttpClient httpClient = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder(new URI(link)).GET().build();
-        HttpResponse<String> response = httpClient
-                .send(request, HttpResponse.BodyHandlers.ofString());
+        HttpRequest request = HttpRequest.newBuilder(new URI(url)).GET().build();
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
-        List<String> images = Arrays.stream(response.body().split("img_src"))
-                .map(line->line.split("\",\"earth_date\"")[0])
-                .map(line->line.substring(3))
+        return extractImageUrls(response.body());
+    }
+
+    @SneakyThrows
+    public static List<String> getByURLConnection(String url) {
+
+        try (var reader = new BufferedReader(
+                new InputStreamReader(new URL(url).openConnection().getInputStream(), StandardCharsets.UTF_8))) {
+            return extractImageUrls(reader.readLine());
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    private static List<String> extractImageUrls(String body) {
+        return Arrays.stream(body.split("img_src\":\""))
+                .map(line -> line.substring(0, line.indexOf(",") - 1))
                 .filter(line -> line.startsWith("http"))
-//                .peek(System.out::println)
                 .collect(Collectors.toList());
-
-        System.out.println(images);
     }
 }
